e9e8ba07da06199fea439e91ffb229c0e840774d
Md. Jahidul Islam <97834703+Jahid234@users.noreply.github.com>
2022-02-21 01:26:30 +0600

    Add files via upload

---
+++ ApiUtilities.java
@@ -0,0 +1,17 @@
+package com.example.covid19app;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiUtilities {
+
+    public static Retrofit retrofit=null;
+
+    public static Apiinterface getAPIInterface(){
+        if (retrofit==null){
+            retrofit=new Retrofit.Builder().baseUrl(Apiinterface.BASE_URL).addConverterFactory(GsonConverterFactory.create()).build();
+        }
+
+        return retrofit.create(Apiinterface.class);
+    }
+}
---
+++ Apiinterface.java
@@ -0,0 +1,14 @@
+package com.example.covid19app;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Apiinterface {
+
+    String BASE_URL="https://corona.lmao.ninja/v2/";
+
+    @GET("countries")
+    Call<List<ModelClass>>getcountrydata();
+}
